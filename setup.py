5bd5eaa6afe8fc103832ab7890987117a6c13dbc
Wenbing Li <libruce@amazon.com>
2018-06-15 23:11:49 -0700

    Release 0.3

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ requires = ['awscli>=1.11.0']
 setup(
     name='awscli-plugin-endpoint',
     packages=['awscli_plugin_endpoint'],
-    version='0.2',
+    version='0.3',
     description='Endpoint plugin for AWS CLI',
     long_description=open('README.md').read(),
-    author='Bruce Li',
+    author='Wenbing Li',
     author_email='wbinglee@gmail.com',
     url='https://github.com/wbinglee/awscli-plugin-endpoint',
-    download_url='https://github.com/wbinglee/awscli-plugin-endpoint/tarball/0.1',
+    download_url='https://github.com/wbinglee/awscli-plugin-endpoint/tarball/0.3',
     keywords=['awscli', 'plugin', 'endpoint'],
     install_requires=requires,
     classifiers = []
